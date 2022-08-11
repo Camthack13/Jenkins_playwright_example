66bd805dd391fcc2beb00e77e0c0dd4e6fe31e28
Cathac13 <camronthackeray@gmail.com>
2022-08-11 10:59:05 -0600

    as

--- tests/jenkins-homepage.spec.ts
+++ tests/jenkins-homepage.spec.ts
@@ -4,7 +4,7 @@ test('Jenkins Homepage', async ({ page}) => {
         await page.goto('https://www.jenkins.io/');
 
         // Expect a title "to contain" a substring.
-        await expect(page).toHaveTitle(/Jenkins/);
+        //await expect(page).toHaveTitle(/Jenkins/);
 
         await page.screenshot({ path: 'homepage.png', fullPage:true });
 });
